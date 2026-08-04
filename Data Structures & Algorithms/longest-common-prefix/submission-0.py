5af9c2ba81eed5bd82f400fd549fe8b93567ca74
Saurabh Pandey <saurabhkumarpandey604@gmail.com>
2026-08-05 04:08:24 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        prefix=strs[0]
+        k=len(prefix)
+        for i in range(1,len(strs)):
+            while not strs[i].startswith(prefix):
+                k-=1
+                prefix=prefix[:k]
+
+
+                if not prefix:
+                    return ""
+        return prefix
+
+            
+               
+                
+
+        
